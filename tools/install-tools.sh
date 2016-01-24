fc12514fec8107e050414faec50eea4210bb0dc4
Julio Sene França <julio.sene@gmail.com>
2016-01-24 19:58:06 -0200

    Create install-tools.sh

---
+++ tools/install-tools.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#
+apt-get install apache2-utils
+
+htpasswd -c -b /usr/share/nginx/html/.htpasswd $1 $2
+
+mkdir /usr/share/nginx/html/tools
+cd /usr/share/nginx/html/tools
+# Download and install PHP File Manager & phpmyadmin
+wget http://sourceforge.net/projects/phpfm/files/phpFileManager/version%200.9.8/phpFileManager-0.9.8.zip/download
+unzip download
+mkdir filemanager
+mv index.php ./filemanager/
+wget https://files.phpmyadmin.net/phpMyAdmin/4.5.3.1/phpMyAdmin-4.5.3.1-all-languages.zip
+unzip phpMyAdmin-4.5.3.1-all-languages.zip
+mv phpMyAdmin-4.5.3.1-all-languages phpmyadmin
+

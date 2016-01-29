75fbfe0a1da18f4d54921180421fb17383aeda91
Julio Sene França <julio.sene@gmail.com>
2016-01-29 16:04:53 -0200

    Update nginx-php-install.sh

--- nginx-php-install.sh
+++ nginx-php-install.sh
@@ -33,19 +33,18 @@ apt-get -y -f install nodejs-legacy
 apt-get -y -f install npm
 npm install -g azure-cli
 
+if hash azure 2>/dev/null; then
+        sleep 2
+#       apt-get -y -f install npm
+        npm install -g azure-cli
+fi
+
 azure storage share create $SharedAzureFileName -a $SharedStorageAccountName -k $SharedStorageAccountKey
 # while [ "`azure storage share list $SharedAzureFileName -a $SharedStorageAccountName -k $SharedStorageAccountKey |grep -q 'html' && echo 'yes'`" != "yes" ]
 # do
 if [ "`azure storage share list $SharedAzureFileName -a $SharedStorageAccountName -k $SharedStorageAccountKey |grep -q 'html' && echo 'yes'`" != "yes" ];
 then
-#        sleep 5
-        if hash azure 2>/dev/null; then
-#             apt-get -y -f install npm
-             npm install -g azure-cli
-        fi
-
         azure storage share create $SharedAzureFileName -a $SharedStorageAccountName -k $SharedStorageAccountKey
-fi
 # done
 
 fi

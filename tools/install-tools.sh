f1487387549a6a5e00ea29437c6d49bba88fafa5
Julio Sene França <julio.sene@gmail.com>
2016-02-02 11:29:47 -0200

    Update install-tools.sh

--- tools/install-tools.sh
+++ tools/install-tools.sh
@@ -11,23 +11,31 @@ mv tools.conf /etc/nginx/conf.d/
 
 mkdir /usr/share/nginx/html/tools
 cd /usr/share/nginx/html/tools
+# Download index page
+wget https://raw.githubusercontent.com/juliosene/azure-nginx-php-mariadb-cluster/master/tools/index.html
+#
 # Download and install PHP File Manager & phpmyadmin
+#
+# File Manager
 wget http://sourceforge.net/projects/phpfm/files/phpFileManager/version%200.9.8/phpFileManager-0.9.8.zip/download
 unzip download
 mkdir filemanager
 mv index.php filemanager/
 mv LICENSE.html filemanager/
-
+#
+# PHPMyAdmin
 wget https://files.phpmyadmin.net/phpMyAdmin/4.5.3.1/phpMyAdmin-4.5.3.1-all-languages.zip
 unzip phpMyAdmin-4.5.3.1-all-languages.zip
 mv phpMyAdmin-4.5.3.1-all-languages phpmyadmin
 
 rm -Rf download phpMyAdmin-4.5.3.1-all-languages.zip
 
+# Config file to PHPMyAdmin
 wget https://raw.githubusercontent.com/juliosene/azure-nginx-php-mariadb-cluster/master/tools/config.inc.php
 mv config.inc.php /usr/share/nginx/html/tools/phpmyadmin/
 sed -i "s/#SECURITY#/$SECURITY/g" /usr/share/nginx/html/tools/phpmyadmin/config.inc.php
-
+#
+# Restart web server
 service nginx restart
 
 

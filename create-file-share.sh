cf4f636b3c41addaa10b41b12246281a59cab264
Julio Sene França <julio.sene@gmail.com>
2016-02-01 23:54:20 -0200

    Update create-file-share.sh

--- create-file-share.sh
+++ create-file-share.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # # Create Azure file share that will be used by front end VM's for moodledata directory
-SharedStorageAccountName=$2
-SharedAzureFileName=$3
-SharedStorageAccountKey=$4
+SharedStorageAccountName=$1
+SharedAzureFileName=$2
+SharedStorageAccountKey=$3
 
 apt-get -y install nodejs-legacy
 apt-get -y install npm

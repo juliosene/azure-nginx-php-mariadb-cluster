691160304ade3842eca7c0178a7bde2f125bceef
Julio Sene França <julio.sene@gmail.com>
2016-02-03 12:20:12 -0200

    php 7 compatibility

--- nginx-php-install.sh
+++ nginx-php-install.sh
@@ -14,16 +14,21 @@ OPTION=${1-2}
 SharedStorageAccountName=$2
 SharedAzureFileName=$3
 SharedStorageAccountKey=$4
-InstallTools=${5:-"no"}
-ToolsUser=$6
-ToolsPass=$7
+PHPVersion=${5-5}
+InstallTools=${6:-"no"}
+ToolsUser=$7
+ToolsPass=$8
 
 wget http://nginx.org/keys/nginx_signing.key
 apt-key add nginx_signing.key
 add-apt-repository "deb http://nginx.org/packages/$DISTRO/ $REL nginx"
 add-apt-repository "deb-src http://nginx.org/packages/$DISTRO/ $REL nginx"
+if [ "$PHPVersion" -eq 7 ]; then
+apt-get install -fy python-software-properties
+LC_ALL=en_US.UTF-8 add-apt-repository ppa:ondrej/php -y
+fi
 
-apt-get update
+apt-get -y update
 
 apt-get install -y -f cifs-utils
 
@@ -44,11 +49,23 @@ fi
 fi
 
 apt-get install -fy nginx
+# # PHP 7
+if [ "$PHPVersion" -eq 7 ]; then
+apt-get install php7.0 php7.0-fpm php7.0-mysql -y
+apt-get install -fy php-apc php7.0-gd
+apt-get --purge autoremove -y
+# replace www-data to nginx into /etc/php/7.0/fpm/pool.d/www.conf
+sed -i 's/www-data/nginx/g' /etc/php/7.0/fpm/pool.d/www.conf
+service php7.0-fpm restart
+# # PHP 5
+else
 apt-get install -fy php5-fpm php5-cli php5-mysql
 apt-get install -fy php-apc php5-gd
 # replace www-data to nginx into /etc/php5/fpm/pool.d/www.conf
 sed -i 's/www-data/nginx/g' /etc/php5/fpm/pool.d/www.conf
 service php5-fpm restart
+fi
+
 # backup default Nginx configuration
 mkdir /etc/nginx/conf-bkp
 cp /etc/nginx/conf.d/default.conf /etc/nginx/conf-bkp/default.conf
@@ -65,13 +82,22 @@ mv nginx.conf /etc/nginx/
 #
 wget https://raw.githubusercontent.com/juliosene/azure-nginx-php-mariadb-cluster/master/files/default.conf
 
+# replace for php7 sock
+if [ "$PHPVersion" -eq 7 ]; then
+sed -i "s,/var/run/php5-fpm.sock,/var/run/php/php7.0-fpm.sock,g" default.conf
+fi
+
 #sed -i "s/#WORKER#/$WORKER/g" nginx.conf
 mv default.conf /etc/nginx/conf.d/
 
 # Memcache client installation
+if [ "$PHPVersion" -eq 7 ]; then
+apt-get install -fy php-memcached
+else
 apt-get install -fy php-pear
 apt-get install -fy php5-dev
 printf "\n" |pecl install -f memcache
+fi
 #
 wget https://raw.githubusercontent.com/juliosene/azure-nginx-php-mariadb-cluster/master/files/memcache.ini
 
@@ -98,5 +124,9 @@ echo -e "<html><title>Azure Nginx PHP</title><body><h2>Your Nginx and PHP are ru
 #
 # Services restart
 #
+if [ "$PHPVersion" -eq 7 ]; then
+service php7.0-fpm restart
+else
 service php5-fpm restart
+if
 service nginx restart
